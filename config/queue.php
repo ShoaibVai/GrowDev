a270da19d046d150bd8ed6464bdd32a6f08b3f41
Shoaib Ibna Omar <omar22205101393@diu.edu.bd>
2026-06-17 11:08:15 +0600

    Fix Heroku deployment and 500 errors

--- config/queue.php
+++ config/queue.php
@@ -94,7 +94,7 @@ return [
     */
 
     'batching' => [
-        'database' => env('DB_CONNECTION', 'sqlite'),
+        'database' => env('DB_CONNECTION', 'pgsql'),
         'table' => 'job_batches',
     ],
 
@@ -113,7 +113,7 @@ return [
 
     'failed' => [
         'driver' => env('QUEUE_FAILED_DRIVER', 'database-uuids'),
-        'database' => env('DB_CONNECTION', 'sqlite'),
+        'database' => env('DB_CONNECTION', 'pgsql'),
         'table' => 'failed_jobs',
     ],
 
